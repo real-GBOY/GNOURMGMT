259c16ce1fcb1122497fcd4e8939d2dd921ca818
mahmoud nayel <mahmmoudnayel2004@gmail.com>
2025-10-27 23:36:28 +0300

    src/shared/config/axios.ts

--- src/shared/config/axios.ts
+++ src/shared/config/axios.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const apiClient = axios.create({
-	baseURL: "https://gnour-back.vercel.app/",
+	baseURL: "https://gnour-back.vercel.app/api",
 	timeout: 10000,
 	headers: {
 		"Content-Type": "application/json",
